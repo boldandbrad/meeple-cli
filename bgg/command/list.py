6118c3ef3bb2694d453aed9c97329b19717fa1f6
Bradley Wojcik <bradleycwojcik@gmail.com>
2022-11-21 11:15:28 -0500

    fix naming conflict in list.py

--- bgg/command/list.py
+++ bgg/command/list.py
@@ -2,7 +2,7 @@ import click
 
 from bgg.util.collection_util import is_collection
 from bgg.util.data_util import get_data
-from bgg.util.output_util import color_weight, color_rating, table
+from bgg.util.output_util import color_rating, color_weight, table
 
 
 @click.command(help="List all games/extensions in a collection.")
@@ -11,17 +11,17 @@ from bgg.util.output_util import color_weight, color_rating, table
 @click.option(
     "-b",
     "--boardgames",
-    "type",
+    "only_include",
     is_flag=True,
-    flag_value="b",
+    flag_value="bg",
     help="Include only boardgames in output.",
 )
 @click.option(
     "-e",
     "--expansions",
-    "type",
+    "only_include",
     is_flag=True,
-    flag_value="e",
+    flag_value="ex",
     help="Include only expansions in output.",
 )
 @click.option("-v", "--verbose", is_flag=True, help="Display additional information.")
@@ -29,7 +29,7 @@ from bgg.util.output_util import color_weight, color_rating, table
 # TODO: add option to run update on the collection prior to list
 # TODO: add option to show grid lines or not in the table
 # TODO: implement paging/scrolling for long lists? not sure how tabulate will like that
-def list_collection(collection: str, type: str, verbose: bool):
+def list_collection(collection: str, only_include: str, verbose: bool):
     if not is_collection(collection):
         print(f"{collection} is not a valid collection")
         return
@@ -44,9 +44,9 @@ def list_collection(collection: str, type: str, verbose: bool):
     boardgames = item_dict["boardgames"]
     expansions = item_dict["expansions"]
 
-    if type == "b":
+    if only_include == "bg":
         out_list = boardgames
-    elif type == "e":
+    elif only_include == "ex":
         out_list = expansions
     else:
         out_list = boardgames + expansions
