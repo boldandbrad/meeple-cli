472193c304ff5d41df878af918ddfa4a2440a263
Bradley Wojcik <bradleycwojcik@gmail.com>
2023-05-17 16:01:45 -0400

    change import user option, add help text

--- src/meeple/command/import_.py
+++ src/meeple/command/import_.py
@@ -6,7 +6,9 @@ from meeple.util.input_util import choice_input
 from meeple.util.message_util import error_msg, info_msg, under_msg
 
 
-def _import_collection(collection_items, collection_name, username, dry_run) -> None:
+def _import_collection(
+    collection_items, collection_name: str, bgg_user: str, dry_run: bool
+) -> None:
     # assign a unique collection name
     collection_name = unique_collection_name(collection_name)
     # show expected changes if import were performed
@@ -23,35 +25,36 @@ def _import_collection(collection_items, collection_name, username, dry_run) -> 
         collection_ids = [item.bgg_id for item in collection_items]
         create_collection(collection_name, to_add_ids=collection_ids)
         info_msg(
-            f"Imported [magenta]{username}[/magenta]'s BoardGameGeek collection [u magenta]{collection_name}[/u magenta] containing {len(collection_items)} items. To update, run [green]meeple update[/green]"
+            f"Imported [magenta]{bgg_user}[/magenta]'s BoardGameGeek collection [u magenta]{collection_name}[/u magenta] containing {len(collection_items)} items. To update, run [green]meeple update[/green]"
         )
 
 
 @click.command(name="import")
-@click.option("--username", required=True)
-@click.option("--dry-run", is_flag=True)
+@click.option("--bgg-user", required=True, help="BoardGameGeek username.")
+@click.option(
+    "--dry-run", is_flag=True, help="Summarize potential changes without persisting."
+)
 @click.help_option("-h", "--help")
-# TODO: add dry-run option to show what collections and items would be created without persisting changes
 # TODO: add single/multiple options or a flag to bypass import method prompt
-def import_(username: str, dry_run: bool) -> None:
+def import_(bgg_user: str, dry_run: bool) -> None:
     """Import BoardGameGeek user collections."""
-    # check that the given username is a valid BoardGameGeek username
-    user = get_bgg_user(username)
+    # check that the given BoardGameGeek username is a valid
+    user = get_bgg_user(bgg_user)
     if not user.user_id:
-        error_msg(f"[yellow]{username}[/yellow] is not a valid BoardGameGeek username.")
+        error_msg(f"[yellow]{bgg_user}[/yellow] is not a valid BoardGameGeek username.")
 
     # get user collection items
-    collection_items = get_bgg_user_collection(username)
+    collection_items = get_bgg_user_collection(bgg_user)
 
     # check that the user collection is not empty
     if not collection_items:
         error_msg(
-            f"Nothing to import. BoardGameGeek user [yellow]{username}[/yellow]'s collection is empty."
+            f"Nothing to import. BoardGameGeek user [yellow]{bgg_user}[/yellow]'s collection is empty."
         )
 
     # prompt to user for import method
     import_method = choice_input(
-        f"Would you like to import [magenta]{username}[/magenta]'s user collection items into a single collection or into multiple by item status?",
+        f"Would you like to import [magenta]{bgg_user}[/magenta]'s user collection items into a single collection or into multiple by item status?",
         ["single", "multiple"],
     )
 
@@ -59,7 +62,7 @@ def import_(username: str, dry_run: bool) -> None:
     match import_method:
         # import single collection with all items
         case "single":
-            _import_collection(collection_items, username, username, dry_run)
+            _import_collection(collection_items, bgg_user, bgg_user, dry_run)
 
         # import a separate collection for each used item status
         case "multiple":
@@ -79,26 +82,26 @@ def import_(username: str, dry_run: bool) -> None:
             wishlist_items = [item for item in collection_items if item.status.wishlist]
 
             if own_items:
-                _import_collection(own_items, f"{username}-own", username, dry_run)
+                _import_collection(own_items, f"{bgg_user}-own", bgg_user, dry_run)
             if prevowned_items:
                 _import_collection(
-                    prevowned_items, f"{username}-prevowned", username, dry_run
+                    prevowned_items, f"{bgg_user}-prevowned", bgg_user, dry_run
                 )
             if fortrade_items:
                 _import_collection(
-                    fortrade_items, f"{username}-fortrade", username, dry_run
+                    fortrade_items, f"{bgg_user}-fortrade", bgg_user, dry_run
                 )
             if wanttoplay_items:
                 _import_collection(
-                    wanttoplay_items, f"{username}-wanttoplay", username, dry_run
+                    wanttoplay_items, f"{bgg_user}-wanttoplay", bgg_user, dry_run
                 )
             if wanttobuy_items:
                 _import_collection(
-                    wanttobuy_items, f"{username}-wanttobuy", username, dry_run
+                    wanttobuy_items, f"{bgg_user}-wanttobuy", bgg_user, dry_run
                 )
             if want_items:
-                _import_collection(want_items, f"{username}-want", username, dry_run)
+                _import_collection(want_items, f"{bgg_user}-want", bgg_user, dry_run)
             if wishlist_items:
                 _import_collection(
-                    wishlist_items, f"{username}-wishlist", username, dry_run
+                    wishlist_items, f"{bgg_user}-wishlist", bgg_user, dry_run
                 )
