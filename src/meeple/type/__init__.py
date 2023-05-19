637588ddb02550dbb3dba184580df883d5fc1e06
Bradley Wojcik <bradleycwojcik@gmail.com>
2023-05-19 14:17:45 -0400

    only import collections that contain items

--- src/meeple/type/__init__.py
+++ src/meeple/type/__init__.py
@@ -1,7 +1,8 @@
-from .bgg_collection_item import BGGCollectionItem
+from .bgg_collection_item import BGG_COLLECTION_STATUSES, BGGCollectionItem
 from .bgg_user import BGGUser
 
 __all__ = (
+    BGG_COLLECTION_STATUSES,
     BGGCollectionItem,
     BGGUser,
 )
